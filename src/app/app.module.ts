8d8b2e2856092dd43b4b7c9d049e81dd3de247f3
Joshua Uduehi <33010247+jUdu97@users.noreply.github.com>
2020-10-21 10:15:39 -0500

    added dialog box for password reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,30 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
-import { MatTooltipModule } from "@angular/material/tooltip";
+
 import { AngularMaterialModule } from "./buttons";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { GameLogin } from "src/login/login.component";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { GameLogin, PassReset } from "src/login/login.component";
+import "hammerjs";
 
 @NgModule({
-  declarations: [AppComponent, GameLogin],
+  declarations: [AppComponent, GameLogin, PassReset],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     MatTooltipModule,
+    MatButtonModule,
+    MatDialogModule,
     ReactiveFormsModule,
     AngularMaterialModule,
     GameLogin
   ],
   providers: [],
+  entryComponents: [PassReset],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,33 +1,52 @@
-import { NgModule, Component, OnInit } from "@angular/core";
+import { NgModule, Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
   ReactiveFormsModule,
   FormGroup,
   FormBuilder,
   AbstractControl
 } from "@angular/forms";
-import { MatTooltipModule, TooltipPosition} from "@angular/material/tooltip";
+import {
+  MatDialog,
+  MatDialogConfig,
+  MatDialogModule,
+  MatDialogRef
+} from "@angular/material/dialog";
+import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatButtonModule } from "@angular/material/button";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @Component({
   selector: "login",
   templateUrl: "login.component.html",
-  styleUrls: ["login.component.css"]
+  styleUrls: ["login.component.css"],
+  encapsulation: ViewEncapsulation.None
 })
 @NgModule({
-  imports: [ReactiveFormsModule,  
-    MatTooltipModule, 
+  imports: [
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatTooltipModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    MatDialogModule
   ]
 })
 export class GameLogin implements OnInit {
   gameForm: FormGroup;
   submitted = false;
-  
- 
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    const dialogRef = this.dialog.open(PassReset, dialogConfig);
+    dialogRef
+      .afterClosed()
+      .subscribe((data) => console.log("Dialog box closed"));
+  }
 
   ngOnInit() {
     this.gameForm = this.formBuilder.group({
@@ -39,12 +58,11 @@ export class GameLogin implements OnInit {
     this.submitted = true;
 
     if (this.gameForm.invalid) {
-      return window.alert("Needs input");
+      return window.alert("Needs valid input");
     }
     console.log(this.gameForm.value);
   }
 }
-
 function ValidateUser(control: AbstractControl): { [key: string]: any } | null {
   if (control.value !== "goship97") {
     return { userValid: true };
@@ -57,4 +75,27 @@ function ValidatePass(control: AbstractControl): { [key: string]: any } | null {
   }
 
   return null;
+}
+
+@Component({
+  selector: "password-reset",
+  templateUrl: "password-reset.html",
+  styleUrls: ["password-reset.css"]
+})
+export class PassReset {
+  constructor(private dialogRef: MatDialogRef<PassReset>) {}
+
+  passwordTitle = "Password Reset";
+  resetInfo: string = "Enter username below: ";
+  sendButtonText: string = "Send";
+  closeButtonText: string = "Close";
+  successMessage: string = "";
+
+  sendInfo(successMessage: string) {
+    console.log("Check your email for the password reset.");
+    this.successMessage = "Email sent!";
+  }
+  onNoClick(): void {
+    this.dialogRef.close(true);
+  }
 }
